3b696310fa38cbc5d7e6f779e554b92d9bbfe94d
Katelyn Peterson <katelynrpetrson16@gmail.com>
2022-06-24 14:41:52 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.post('/', (req, res) => {
+    console.log('receiving data ...');
+    console.log('body is ', req.body);
+    res.send('uploaded file');
+})
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+})
