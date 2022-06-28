e66c33008800fb269d782e3fd386d9524dbbd03d
Katelyn Peterson <katelynrpeterson16@gmail.com>
2022-06-27 23:15:00 -0400

    Add initial config

--- app.js
+++ app.js
@@ -4,11 +4,24 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+const { MongoClient } = require('mongodb');
+const uri = "mongodb://localhost:27017/test";
+const client = new MongoClient(uri);
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+//query parameters
+app.get('/', async (req, res) => {
+    await client.connect();
+    let databasesList = await client.db().admin().listDatabases();
+    console.log(databasesList)
+    res.send('Hello World!' + req.query.name);
+})
+
+//path params
+app.get('/:name', (req, res) => {
+    res.send('Hello World! '+ req.params.name);
 })
 
 app.post('/', (req, res) => {
